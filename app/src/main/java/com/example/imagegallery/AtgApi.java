e2f391acbfcfe4345d3503a2b3b9516a4a5e7dda
swapnil-railkar <74483977+swapnil-railkar@users.noreply.github.com>
2021-05-14 22:35:19 +0530

    Refactoring

--- app/src/main/java/com/example/imagegallery/AtgApi.java
+++ app/src/main/java/com/example/imagegallery/AtgApi.java
@@ -1,12 +1,9 @@
 package com.example.imagegallery;
 
-import android.content.ClipData;
-
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 //Class to access API
 public class AtgApi {
@@ -18,25 +15,23 @@ public class AtgApi {
     public static final String url = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=20&page=1";
 
     //Using singleton pattern to make an object of retrofit exactly once
-    public static PostService postService=null;
+    public static PostService postService = null;
 
-    public static PostService getService()
-    {
-        if(postService == null)
-        {
+    public static PostService getService() {
+        if (postService == null) {
             //Create PostService
-            Retrofit retrofit=new Retrofit.Builder()
-                     .baseUrl(url)
-                     .addConverterFactory(GsonConverterFactory.create())
-                     .build();
-            postService=retrofit.create(PostService.class);
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            postService = retrofit.create(PostService.class);
         }
         return postService;
     }
 
     public interface PostService {
         //Method to return posts of given URL
-        @GET(url+"&api_key=" + key+"&format=json&nojsoncallback=1&extras=url_s")
+        @GET(url + "&api_key=" + key + "&format=json&nojsoncallback=1&extras=url_s")
         Call<PostList> getPostList();
     }
 
